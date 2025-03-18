513eec09403afa949e5f796b5ea9a51420673157
1cywind <nguyenkhoi200455@gmail.com>
2025-03-18 19:15:42 +0700

    Test again

--- frontend/src/pages/SuccessfulPage.jsx
+++ frontend/src/pages/SuccessfulPage.jsx
@@ -6,7 +6,7 @@ const SuccessfulPage = () => {
             <div className="mx-auto max-w-7xl p-10 flex flex-col items-center gap-10">
                 <div className="text-center">
                     <p>Your order has been sent successfully!</p>
-                    <p>Please wait for a call from our delivery person!</p>
+                    <p>Please wait for a call from our delivery person to receive your order!</p>
                 </div>
                 <img src="./assets/delivery.jpg" alt="Delivery" />
             </div>
